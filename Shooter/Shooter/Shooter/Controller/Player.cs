69dea76e1a2e2a56a165a0ca2cfbbbfdbf451e4d
1upsoda <1upsoda@gmail.com>
2015-05-08 07:22:55 -0600

    starts getting it to myself

---
+++ Shooter/Shooter/Shooter/Controller/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Shooter.View;
+namespace Shooter.Controller
+{
+    class Player
+    {
+        public Animation PlayerAnimation;
+        public Vector2 Position;
+        public bool Active;
+        public int Health;
+        public int Width
+        {
+            get { return PlayerAnimation.FrameWidth; }
+        }
+
+        // Get the height of the player ship
+        public int Height
+        {
+            get { return PlayerAnimation.FrameHeight; }
+        }
+        public void Initialize(Animation animation, Vector2 position)
+        {
+            PlayerAnimation = animation;
+
+            // Set the starting position of the player around the middle of the screen and to the back
+            Position = position;
+
+            // Set the player to be active
+            Active = true;
+
+            // Set the player health
+            Health = 100;
+        }
+        public void Update(GameTime gameTime)
+        {
+            PlayerAnimation.Position = Position;
+            PlayerAnimation.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            PlayerAnimation.Draw(spriteBatch);
+        }
+
+    }
+}
